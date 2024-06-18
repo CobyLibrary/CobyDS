4bda0f520b3bac27ece381500f4f7587987e0281
Coby <coby5502@gmail.com>
2024-06-18 22:43:09 +0900

    사이즈 변경

--- Sources/CobyDS/Extensions/BaseSize.swift
+++ Sources/CobyDS/Extensions/BaseSize.swift
@@ -12,7 +12,9 @@ public struct BaseSize {
     public static let screenWidth: CGFloat = UIScreen.main.bounds.size.width
     public static let screenHeight: CGFloat = UIScreen.main.bounds.size.height
     public static let fullWidth: CGFloat = screenWidth - horizantalPadding * 2
-    public static let cellWidth: CGFloat = (fullWidth - horizantalPadding) / 2
+    public static let cellHorizantalSpacing: CGFloat = 8
+    public static let cellVerticalSpacing: CGFloat = 8
+    public static let cellWidth: CGFloat = (fullWidth - cellHorizantalSpacing) / 2
     
     public static var topAreaPadding: CGFloat {
         let scenes = UIApplication.shared.connectedScenes
