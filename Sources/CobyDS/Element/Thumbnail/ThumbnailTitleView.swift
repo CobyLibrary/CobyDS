de04e1fe63d14cc91dec9846758ceb1f5cf0f840
Coby <coby5502@gmail.com>
2024-06-21 22:02:55 +0900

    폰트 수정

--- Sources/CobyDS/Element/Thumbnail/ThumbnailTitleView.swift
+++ Sources/CobyDS/Element/Thumbnail/ThumbnailTitleView.swift
@@ -11,18 +11,15 @@ public struct ThumbnailTitleView: View {
     
     private let image: UIImage?
     private let title: String
-    private let description: String
     private let isShadowing: Bool
     
     public init(
         image: UIImage? = nil,
         title: String,
-        description: String,
         isShadowing: Bool = false
     ) {
         self.image = image
         self.title = title
-        self.description = description
         self.isShadowing = isShadowing
     }
     
@@ -34,19 +31,11 @@ public struct ThumbnailTitleView: View {
             )
             .aspectRatio(1.0, contentMode: .fit)
             
-            VStack(alignment: .leading, spacing: 2) {
-                Text(self.title)
-                    .font(.pretendard(size: 15, weight: .semibold))
-                    .foregroundColor(Color.labelNormal)
-                    .lineLimit(1)
-                    .truncationMode(.tail)
-                
-                Text(self.description)
-                    .font(.pretendard(size: 12, weight: .medium))
-                    .foregroundColor(Color.labelAlternative)
-                    .lineLimit(1)
-                    .truncationMode(.tail)
-            }
+            Text(self.title)
+                .font(.pretendard(size: 16, weight: .medium))
+                .foregroundColor(Color.labelNormal)
+                .lineLimit(1)
+                .truncationMode(.tail)
         }
     }
 }
@@ -56,14 +45,12 @@ public struct ThumbnailTitleView: View {
         ThumbnailTitleView(
             image: UIImage.icImage,
             title: "제목입니다만",
-            description: "사진에 대한 설명이에요",
             isShadowing: true
         )
         .frame(width: 150)
         
         ThumbnailTitleView(
-            title: "제목입니다만",
-            description: "설명잉에요"
+            title: "제목입니다만"
         )
         .frame(width: 150)
     }
