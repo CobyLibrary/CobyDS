40b70e96ca998323a91c6db6a2081ed1258f43e1
coby <coby5502@gmail.com>
2024-08-16 17:35:10 +0900

    다크 삭제

--- Sources/CobyDS/Theme/Primary.swift
+++ Sources/CobyDS/Theme/Primary.swift
@@ -9,109 +9,48 @@ import SwiftUI
 
 extension Color {
     
-    // SwiftUI Environment property to detect color scheme
-    @Environment(\.colorScheme) private static var colorScheme
-    
     // Blue
-    public static var blueNormal: Color {
-        return colorScheme == .dark ? Color.blue60 : Color.blue50
-    }
-    
-    public static var blueStrong: Color {
-        return colorScheme == .dark ? Color.blue55 : Color.blue45
-    }
-    
-    public static var blueHeavy: Color {
-        return colorScheme == .dark ? Color.blue50 : Color.blue40
-    }
+    public static let blueNormal: Color = Color.blue50
+    public static let blueStrong: Color = Color.blue45
+    public static let blueHeavy: Color = Color.blue40
     
     // Red
-    public static var redNormal: Color {
-        return colorScheme == .dark ? Color.red60 : Color.red50
-    }
-    
-    public static var redHeavy: Color {
-        return colorScheme == .dark ? Color.red50 : Color.red40
-    }
+    public static let redNormal: Color = Color.red50
+    public static let redHeavy: Color = Color.red40
     
     // Green
-    public static var greenNormal: Color {
-        return colorScheme == .dark ? Color.green60 : Color.green50
-    }
-    
-    public static var greenHeavy: Color {
-        return colorScheme == .dark ? Color.green50 : Color.green40
-    }
+    public static let greenNormal: Color = Color.green50
+    public static let greenHeavy: Color = Color.green40
     
     // Orange
-    public static var orangeNormal: Color {
-        return colorScheme == .dark ? Color.orange60 : Color.orange50
-    }
-    
-    public static var orangeHeavy: Color {
-        return colorScheme == .dark ? Color.orange50 : Color.orange40
-    }
+    public static let orangeNormal: Color = Color.orange50
+    public static let orangeHeavy: Color = Color.orange40
     
     // RedOrange
-    public static var redOrangeNormal: Color {
-        return colorScheme == .dark ? Color.redOrange60 : Color.redOrange50
-    }
-    
-    public static var redOrangeHeavy: Color {
-        return colorScheme == .dark ? Color.redOrange50 : Color.redOrange40
-    }
+    public static let redOrangeNormal: Color = Color.redOrange50
+    public static let redOrangeHeavy: Color = Color.redOrange40
     
     // Lime
-    public static var limeNormal: Color {
-        return colorScheme == .dark ? Color.lime60 : Color.lime50
-    }
-    
-    public static var limeHeavy: Color {
-        return colorScheme == .dark ? Color.lime50 : Color.lime40
-    }
+    public static let limeNormal: Color = Color.lime50
+    public static let limeHeavy: Color = Color.lime40
     
     // Cyan
-    public static var cyanNormal: Color {
-        return colorScheme == .dark ? Color.cyan60 : Color.cyan50
-    }
-    
-    public static var cyanHeavy: Color {
-        return colorScheme == .dark ? Color.cyan50 : Color.cyan40
-    }
+    public static let cyanNormal: Color = Color.cyan50
+    public static let cyanHeavy: Color = Color.cyan40
     
     // LightBlue
-    public static var lightBlueNormal: Color {
-        return colorScheme == .dark ? Color.lightBlue60 : Color.lightBlue50
-    }
-    
-    public static var lightBlueHeavy: Color {
-        return colorScheme == .dark ? Color.lightBlue50 : Color.lightBlue40
-    }
+    public static let lightBlueNormal: Color = Color.lightBlue50
+    public static let lightBlueHeavy: Color = Color.lightBlue40
     
     // Violet
-    public static var violetNormal: Color {
-        return colorScheme == .dark ? Color.violet60 : Color.violet50
-    }
-    
-    public static var violetHeavy: Color {
-        return colorScheme == .dark ? Color.violet50 : Color.violet40
-    }
+    public static let violetNormal: Color = Color.violet50
+    public static let violetHeavy: Color = Color.violet40
     
     // Purple
-    public static var purpleNormal: Color {
-        return colorScheme == .dark ? Color.purple60 : Color.purple50
-    }
-    
-    public static var purpleHeavy: Color {
-        return colorScheme == .dark ? Color.purple50 : Color.purple40
-    }
+    public static let purpleNormal: Color = Color.purple50
+    public static let purpleHeavy: Color = Color.purple40
     
     // Pink
-    public static var pinkNormal: Color {
-        return colorScheme == .dark ? Color.pink60 : Color.pink50
-    }
-    
-    public static var pinkHeavy: Color {
-        return colorScheme == .dark ? Color.pink50 : Color.pink40
-    }
+    public static let pinkNormal: Color = Color.pink50
+    public static let pinkHeavy: Color = Color.pink40
 }
--- Sources/CobyDS/Theme/Semantic.swift
+++ Sources/CobyDS/Theme/Semantic.swift
@@ -14,168 +14,60 @@ extension Color {
     public static let staticBlack = Color.common0
     
     // Label
-    public static var labelNormal: Color {
-        return colorScheme == .dark ? Color.coolNeutral99 : Color.coolNeutral10
-    }
-    
-    public static var labelStrong: Color {
-        return colorScheme == .dark ? Color.common100 : Color.common0
-    }
-    
-    public static var labelNeutral: Color {
-        return colorScheme == .dark ? Color.coolNeutral90.opacity(0.88) : Color.coolNeutral22.opacity(0.88)
-    }
-    
-    public static var labelAlternative: Color {
-        return colorScheme == .dark ? Color.coolNeutral80.opacity(0.61) : Color.coolNeutral25.opacity(0.61)
-    }
-    
-    public static var labelAssistive: Color {
-        return colorScheme == .dark ? Color.coolNeutral80.opacity(0.28) : Color.coolNeutral25.opacity(0.28)
-    }
-    
-    public static var labelDisable: Color {
-        return colorScheme == .dark ? Color.coolNeutral70.opacity(0.16) : Color.coolNeutral25.opacity(0.16)
-    }
+    public static let labelNormal: Color = Color.coolNeutral10
+    public static let labelStrong: Color = Color.common0
+    public static let labelNeutral: Color = Color.coolNeutral22.opacity(0.88)
+    public static let labelAlternative: Color = Color.coolNeutral25.opacity(0.61)
+    public static let labelAssistive: Color = Color.coolNeutral25.opacity(0.28)
+    public static let labelDisable: Color = Color.coolNeutral25.opacity(0.16)
     
     // Background
-    public static var backgroundNormalNormal: Color {
-        return colorScheme == .dark ? Color.coolNeutral15 : Color.common100
-    }
-    
-    public static var backgroundNormalAlternative: Color {
-        return colorScheme == .dark ? Color.coolNeutral5 : Color.coolNeutral99
-    }
-    
-    public static var backgroundElevatedNormal: Color {
-        return colorScheme == .dark ? Color.coolNeutral17 : Color.common100
-    }
-    
-    public static var backgroundElevatedAlternative: Color {
-        return colorScheme == .dark ? Color.coolNeutral7 : Color.coolNeutral99
-    }
+    public static let backgroundNormalNormal: Color = Color.common100
+    public static let backgroundNormalAlternative: Color = Color.coolNeutral99
+    public static let backgroundElevatedNormal: Color = Color.common100
+    public static let backgroundElevatedAlternative: Color = Color.coolNeutral99
     
     // Interaction
-    public static var interactionInactive: Color {
-        return colorScheme == .dark ? Color.coolNeutral40 : Color.coolNeutral70
-    }
-    
-    public static var interactionDisable: Color {
-        return colorScheme == .dark ? Color.coolNeutral22 : Color.coolNeutral98
-    }
+    public static let interactionInactive: Color = Color.coolNeutral70
+    public static let interactionDisable: Color = Color.coolNeutral98
     
     // Line
-    public static var lineNormalNormal: Color {
-        return colorScheme == .dark ? Color.coolNeutral50.opacity(0.32) : Color.coolNeutral50.opacity(0.22)
-    }
-    
-    public static var lineNormalNeutral: Color {
-        return colorScheme == .dark ? Color.coolNeutral50.opacity(0.28) : Color.coolNeutral50.opacity(0.16)
-    }
-    
-    public static var lineNormalAlternative: Color {
-        return colorScheme == .dark ? Color.coolNeutral50.opacity(0.22) : Color.coolNeutral50.opacity(0.08)
-    }
-    
-    public static var lineSolidNormal: Color {
-        return colorScheme == .dark ? Color.coolNeutral25 : Color.coolNeutral96
-    }
-    
-    public static var lineSolidNeutral: Color {
-        return colorScheme == .dark ? Color.coolNeutral23 : Color.coolNeutral97
-    }
-    
-    public static var lineSolidAlternative: Color {
-        return colorScheme == .dark ? Color.coolNeutral22 : Color.coolNeutral98
-    }
+    public static let lineNormalNormal: Color = Color.coolNeutral50.opacity(0.22)
+    public static let lineNormalNeutral: Color = Color.coolNeutral50.opacity(0.16)
+    public static let lineNormalAlternative: Color = Color.coolNeutral50.opacity(0.08)
+    public static let lineSolidNormal: Color = Color.coolNeutral96
+    public static let lineSolidNeutral: Color = Color.coolNeutral97
+    public static let lineSolidAlternative: Color = Color.coolNeutral98
     
     // Status
-    public static var statusPositive: Color {
-        return colorScheme == .dark ? Color.green60 : Color.green50
-    }
-    
-    public static var statusCautionary: Color {
-        return colorScheme == .dark ? Color.orange60 : Color.orange50
-    }
-    
-    public static var statusNegative: Color {
-        return colorScheme == .dark ? Color.red60 : Color.red50
-    }
+    public static let statusPositive: Color = Color.green50
+    public static let statusCautionary: Color = Color.orange50
+    public static let statusNegative: Color = Color.red50
     
     // Accent
-    public static var accentLime: Color {
-        return colorScheme == .dark ? Color.lime60 : Color.lime50
-    }
-    
-    public static var accentCyan: Color {
-        return colorScheme == .dark ? Color.cyan60 : Color.cyan50
-    }
-    
-    public static var accentLightBlue: Color {
-        return colorScheme == .dark ? Color.lightBlue60 : Color.lightBlue50
-    }
-    
-    public static var accentViolet: Color {
-        return colorScheme == .dark ? Color.violet60 : Color.violet50
-    }
-    
-    public static var accentPurple: Color {
-        return colorScheme == .dark ? Color.purple60 : Color.purple50
-    }
-    
-    public static var accentPink: Color {
-        return colorScheme == .dark ? Color.pink60 : Color.pink50
-    }
+    public static let accentLime: Color = Color.lime50
+    public static let accentCyan: Color = Color.cyan50
+    public static let accentLightBlue: Color = Color.lightBlue50
+    public static let accentViolet: Color = Color.violet50
+    public static let accentPurple: Color = Color.purple50
+    public static let accentPink: Color = Color.pink50
     
     // Inverse
-    public static var inversePrimary: Color {
-        return colorScheme == .dark ? Color.blue50 : Color.blue60
-    }
-    
-    public static var inverseBackground: Color {
-        return colorScheme == .dark ? Color.common100 : Color.coolNeutral15
-    }
-    
-    public static var inverseLabel: Color {
-        return colorScheme == .dark ? Color.coolNeutral10 : Color.coolNeutral99
-    }
+    public static let inversePrimary: Color = Color.blue60
+    public static let inverseBackground: Color = Color.coolNeutral15
+    public static let inverseLabel: Color = Color.coolNeutral99
     
     // Fill
-    public static var fillNormal: Color {
-        return colorScheme == .dark ? Color.coolNeutral50.opacity(0.22) : Color.coolNeutral50.opacity(0.08)
-    }
-    
-    public static var fillStrong: Color {
-        return colorScheme == .dark ? Color.coolNeutral50.opacity(0.28) : Color.coolNeutral50.opacity(0.16)
-    }
-    
-    public static var fillAlternative: Color {
-        return colorScheme == .dark ? Color.coolNeutral50.opacity(0.12) : Color.coolNeutral50.opacity(0.05)
-    }
+    public static let fillNormal: Color = Color.coolNeutral50.opacity(0.08)
+    public static let fillStrong: Color = Color.coolNeutral50.opacity(0.16)
+    public static let fillAlternative: Color = Color.coolNeutral50.opacity(0.05)
     
     // Material
-    public static var meterialDimmer: Color {
-        return colorScheme == .dark ? Color.coolNeutral10.opacity(0.74) : Color.coolNeutral10.opacity(0.52)
-    }
+    public static let meterialDimmer: Color = Color.coolNeutral10.opacity(0.52)
     
     // Shadow
-    public static var shadowNormal: Color {
-        return colorScheme == .dark ? Color.staticWhite.opacity(0.16) : Color.staticBlack.opacity(0.16)
-    }
-    
-    public static var shadowEmphasize: Color {
-        return colorScheme == .dark ? Color.staticWhite.opacity(0.22) : Color.staticBlack.opacity(0.22)
-    }
-    
-    public static var shadowStrong: Color {
-        return colorScheme == .dark ? Color.staticWhite.opacity(0.22) : Color.staticBlack.opacity(0.22)
-    }
-    
-    public static var shadowHeavy: Color {
-        return colorScheme == .dark ? Color.staticWhite.opacity(0.28) : Color.staticBlack.opacity(0.28)
-    }
-}
-
-private extension Color {
-    @Environment(\.colorScheme) private static var colorScheme
+    public static let shadowNormal: Color = Color.staticBlack.opacity(0.16)
+    public static let shadowEmphasize: Color = Color.staticBlack.opacity(0.22)
+    public static let shadowStrong: Color = Color.staticBlack.opacity(0.22)
+    public static let shadowHeavy: Color = Color.staticBlack.opacity(0.28)
 }
